adce20f4c66cfaa6feb5cd54429e0d78ff33143a
Eugenio araújo <eugeniodornelesl2@gmail.com>
2023-01-22 11:05:28 -0300

    feat(): RankingScreen Done

--- App.tsx
+++ App.tsx
@@ -1,27 +1,27 @@
-import "react-native-gesture-handler";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { AuthNavigator } from "./src/routes/stacks/AuthStack";
-import { useFonts } from "expo-font";
-import { ThemeProvider } from "styled-components/native";
-import { theme } from "./src/constants";
-import { Routes } from "./src/routes";
-import { AuthProvider } from "./src/context/authProvider";
+import 'react-native-gesture-handler'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { AuthNavigator } from './src/routes/stacks/AuthStack'
+import { useFonts } from 'expo-font'
+import { ThemeProvider } from 'styled-components/native'
+import { theme } from './src/constants'
+import { Routes } from './src/routes'
+import { AuthProvider } from './src/context/authProvider'
 
-const { Screen, Navigator } = createStackNavigator();
+const { Screen, Navigator } = createStackNavigator()
 
 export default function App() {
   const [loaded] = useFonts({
-    InterBold: require("./src/assets/fonts/Inter-Bold.ttf"),
-    InterSemiBold: require("./src/assets/fonts/Inter-SemiBold.ttf"),
-    InterMedium: require("./src/assets/fonts/Inter-Medium.ttf"),
-    InterRegular: require("./src/assets/fonts/Inter-Regular.ttf"),
-    InterLight: require("./src/assets/fonts/Inter-Light.ttf"),
-    Montserrat: require("./src/assets/fonts/Montserrat-VariableFont_wght.ttf"),
-  });
+    InterBold: require('./src/assets/fonts/Inter-Bold.ttf'),
+    InterSemiBold: require('./src/assets/fonts/Inter-SemiBold.ttf'),
+    InterMedium: require('./src/assets/fonts/Inter-Medium.ttf'),
+    InterRegular: require('./src/assets/fonts/Inter-Regular.ttf'),
+    InterLight: require('./src/assets/fonts/Inter-Light.ttf'),
+    Montserrat: require('./src/assets/fonts/Montserrat-VariableFont_wght.ttf')
+  })
 
-  if (!loaded) return null;
+  if (!loaded) return null
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaProvider>
@@ -32,5 +32,5 @@ export default function App() {
         </NavigationContainer>
       </SafeAreaProvider>
     </ThemeProvider>
-  );
+  )
 }
---
+++ src/components/rankingScrollViewItem/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+
+  padding: 8px;
+  margin-top: 12px;
+  width: 100%;
+  /* background: ${({ theme }) => theme.COLORS.gray_100}; */
+  border-radius: 8px;
+  border-bottom-width: 1px;
+  border-bottom-color: ${({ theme }) => theme.COLORS.secondary_500};
+`
+
+export const CustomImage = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: 64px;
+  height: 64px;
+  border-radius: 32px;
+  margin-right: 8px;
+  background: ${({ theme }) => theme.COLORS.primary_500};
+`
+
+export const Content = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-left: 8px;
+`
+
+export const TextContainer = styled.View``
+
+export const CustomText = styled.Text`
+  font-family: ${({ theme }) => theme.FONTS.intermedium};
+`
